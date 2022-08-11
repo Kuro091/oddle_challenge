84f4ecd89941f933688104696f22dc6f5567a3be
Kuro091 <lcminhhp@gmail.com>
2022-08-12 04:33:46 +0700

    x

--- utils/octokitHelper.js
+++ utils/octokitHelper.js
@@ -1,4 +1,5 @@
 const { Octokit } = require("octokit");
+console.log(' token is', process.env.NEXT_PUBLIC_GITHUB_TOKEN);
 
 const octokit = new Octokit({
   auth: process.env.NEXT_PUBLIC_GITHUB_TOKEN
