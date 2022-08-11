252b171a624a9ac59cac6c808620ddcb832ff582
Kuro091 <lcminhhp@gmail.com>
2022-08-12 04:38:54 +0700

    remove log

--- utils/octokitHelper.js
+++ utils/octokitHelper.js
@@ -1,6 +1,4 @@
 const { Octokit } = require("octokit");
-console.log(' token is', process.env.NEXT_PUBLIC_GITHUB_TOKEN);
-
 const octokit = new Octokit({
   auth: process.env.NEXT_PUBLIC_GITHUB_TOKEN
 });
